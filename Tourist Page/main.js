55f4d8ddbf500796b3f30626eeea9a306ff403d6
Fredrick Barfield <flbarfield@yahoo.com>
2016-05-12 08:23:32 -0700

    added weather widget and wrapped buttons in prep for js

---
+++ Tourist Page/main.js
@@ -0,0 +1,10 @@
+// ICON BEHAIVIOR
+
+//$('#bill-icons i, #city-directory i').on({
+//    'mouseenter': function () {
+//        'use strict';
+//    },
+//    'mouseleave': function () {
+//        'use strict';
+//    }
+//}
