bbf29b903abd197f9b1a1534b05352c73c748fee
Fredrick Barfield <flbarfield@yahoo.com>
2016-10-18 07:27:52 -0700

    deleted unneeded files. Beginning to implement AJAX

--- Swing_Dance/main.js
+++ Swing_Dance/main.js
@@ -1,9 +1,23 @@
 'use strict';
-//	
-//$(function () {
-//	$('.rslides').responsiveSlides({
-//		auto: true,
-//		speed: 500,
-//		timeout: 4000
-//	});
-//});
+
+var xhr;
+
+if (window.XMLHttpRequest) {
+	xhr = new XMLHttpRequest();
+} else if (window.ActiveXOject) {
+	xhr = new ActiveXObject('Msxm122.XMLHTTP');
+} else {
+	throw new Error('Ajax is not supported by your browser');
+}
+
+xhr.onreadystatechange = function () {
+	if (xhr.readyState < 4) {
+		document.getElementById('updatable-content').innerHTML = 'Loading...';
+	} else if (xhr.readystate === 4) {
+		if (xhr.status === 200 && xhr.status < 300) {
+			document.getElementById('updatable-content').innerHTML = xhr.responseText;
+		}
+	}
+	xhr.open('GET', 'front.html');
+	xhr.send(null);
+};
