17029f2e1df579ef9ae2b9e9ac3e0d80b87db8db
Fredrick Barfield <flbarfield@yahoo.com>
2016-10-22 18:33:27 -0700

    discovered the USE OF TRANSITIONS. WOW. Also still working on nav animation

--- Company_Page/main.js
+++ Company_Page/main.js
@@ -1,3 +1,11 @@
-$('.top-jumb fa-navicon').onclick(function (){
-	$('#hidden-nav').toggleClass('hidden');
-})
+'use strict';
+
+function openNav() {
+	document.getElementById('hidden-nav').style.width = '14em';
+	document.getElementById('main-content').style.marginRight = '14em';
+}
+
+function closeNav() {
+	document.getElementbyId('hidden-nav').style.width = '0';
+	document.getElementById('main-content').style.marginRight = '0';
+}
