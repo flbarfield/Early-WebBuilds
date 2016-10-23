5563c8271ee89ff603f47a676e9cf065dba20550
Fredrick Barfield <flbarfield@yahoo.com>
2016-10-23 11:24:40 -0700

    beginning a revamp on the portfolio...

---
+++ My_personal_site/main.js
@@ -0,0 +1,29 @@
+var xhr;
+
+if (window.XMLHttpRequest) {
+	xhr = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+	xhr = new ActiveXObject('MSXM12.XMLHTTP');
+} else {
+	throw new Error('Ajax is not supported by your browser');
+}
+
+$('#company').click(function (e) {
+	$('#updatable-content').load('Additional_Info/Company/about_company.html');
+});
+
+$('#tsdc').click(function (e) {
+	$('#updatable-content').load('Additional_Info/TSDC/about_tsdc.html');
+});
+
+$('#tourist').click(function (e) {
+	$('#updatable-content').load('Additional_Info/TSDC/about_travel.html');
+});
+
+$('#fila').click(function (e) {
+	$('#updatable-content').load('Additional_Info/TSDC/about_fila.html');
+});
+
+$('#blog').click(function (e) {
+	$('#updatable-content').load('Additional_Info/TSDC/about_tsdc.html');
+});
