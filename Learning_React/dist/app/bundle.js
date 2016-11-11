4e99398bde0f2fa0c187164253d759d1e08de51e
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-11 13:48:02 -0700

    passing data with props

--- Learning_React/dist/app/bundle.js
+++ Learning_React/dist/app/bundle.js
@@ -59,7 +59,7 @@
 	
 	var _Header = __webpack_require__(/*! ./components/Header */ 172);
 	
-	var _Home = __webpack_require__(/*! ./components/Home */ 173);
+	var _Home = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./components/Home\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
@@ -84,6 +84,10 @@
 		_createClass(App, [{
 			key: "render",
 			value: function render() {
+				var user = {
+					name: "Anna",
+					hobbies: ["Sports"]
+				};
 				return _react2.default.createElement(
 					"div",
 					{ className: "container" },
@@ -102,6 +106,7 @@
 						_react2.default.createElement(
 							"div",
 							{ className: "col-xs-10 col-xs-offset-1" },
+							_react2.default.createElement(_Home.Home, { name: "Max", age: 27, user: user }),
 							_react2.default.createElement(_Home.Home, null)
 						)
 					)
@@ -22063,61 +22068,6 @@
 		}]);
 	
 		return Header;
-	}(_react2.default.Component);
-
-/***/ },
-/* 173 */
-/*!************************************!*\
-  !*** ./src/app/components/Home.js ***!
-  \************************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-	
-	Object.defineProperty(exports, "__esModule", {
-		value: true
-	});
-	exports.Home = undefined;
-	
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
-	var _react = __webpack_require__(/*! react */ 1);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
-	var Home = exports.Home = function (_React$Component) {
-		_inherits(Home, _React$Component);
-	
-		function Home() {
-			_classCallCheck(this, Home);
-	
-			return _possibleConstructorReturn(this, (Home.__proto__ || Object.getPrototypeOf(Home)).apply(this, arguments));
-		}
-	
-		_createClass(Home, [{
-			key: "render",
-			value: function render() {
-				return _react2.default.createElement(
-					"div",
-					null,
-					_react2.default.createElement(
-						"p",
-						null,
-						"In a new Component!"
-					)
-				);
-			}
-		}]);
-	
-		return Home;
 	}(_react2.default.Component);
 
 /***/ }
--- Learning_React/src/app/components/Home.js
+++ Learning_React/src/app/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export class Home extends React.Component {
 	render() {
+		var text="Something!";
 //		A complex check just before the return funct
 //		is acceptable. The final calc for the answer can
 //		be IN the return within the {}, but it must be a 
@@ -9,8 +10,27 @@ export class Home extends React.Component {
 		return(
 			<div>
 				<p>In a new Component!</p>
-				{"Hello!"}
+				<p>{text}</p>
+				<p>Your name is {this.props.name}, your age is {this.props.age}</p>
+				<p>User Object => Name: {this.props.user.name}</p>
+				<div>
+					<h4>Hobbies</h4>
+					<ul>
+						{this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+					</ul>
+				</div>
+				<hr/>
+				{this.props.children}
 			</div>
 		);
 	}
 }
+
+//PropTypes enforce that your components get used correctly
+
+Home.propTypes = {
+	name: React.PropTypes.string,
+	age: React.PropTypes.number,
+	user: React.PropTypes.object,
+	children: React.PropTypes.element.isRequired
+};
