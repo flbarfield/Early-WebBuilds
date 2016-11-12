b854e8833abda81aa8b7035bf2b5cdd4b925df20
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-11 19:24:43 -0700

    events

--- Learning_React/dist/app/bundle.js
+++ Learning_React/dist/app/bundle.js
@@ -59,7 +59,7 @@
 	
 	var _Header = __webpack_require__(/*! ./components/Header */ 172);
 	
-	var _Home = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./components/Home\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _Home = __webpack_require__(/*! ./components/Home */ 173);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
@@ -86,7 +86,7 @@
 			value: function render() {
 				var user = {
 					name: "Anna",
-					hobbies: ["Sports"]
+					hobbies: ["Sports", "Reading"]
 				};
 				return _react2.default.createElement(
 					"div",
@@ -106,8 +106,15 @@
 						_react2.default.createElement(
 							"div",
 							{ className: "col-xs-10 col-xs-offset-1" },
-							_react2.default.createElement(_Home.Home, { name: "Max", age: 27, user: user }),
-							_react2.default.createElement(_Home.Home, null)
+							_react2.default.createElement(
+								_Home.Home,
+								{ name: "Max", age: 27, user: user },
+								_react2.default.createElement(
+									"p",
+									null,
+									"This is a paragraph!"
+								)
+							)
 						)
 					)
 				);
@@ -22069,6 +22076,116 @@
 	
 		return Header;
 	}(_react2.default.Component);
+
+/***/ },
+/* 173 */
+/*!************************************!*\
+  !*** ./src/app/components/Home.js ***!
+  \************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.Home = undefined;
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Home = exports.Home = function (_React$Component) {
+		_inherits(Home, _React$Component);
+	
+		function Home() {
+			_classCallCheck(this, Home);
+	
+			return _possibleConstructorReturn(this, (Home.__proto__ || Object.getPrototypeOf(Home)).apply(this, arguments));
+		}
+	
+		_createClass(Home, [{
+			key: "render",
+			value: function render() {
+				var text = "Something!";
+				//		A complex check just before the return funct
+				//		is acceptable. The final calc for the answer can
+				//		be IN the return within the {}, but it must be a 
+				//		single line
+				return _react2.default.createElement(
+					"div",
+					null,
+					_react2.default.createElement(
+						"p",
+						null,
+						"In a new Component!"
+					),
+					_react2.default.createElement(
+						"p",
+						null,
+						text
+					),
+					_react2.default.createElement(
+						"p",
+						null,
+						"Your name is ",
+						this.props.name,
+						", your age is ",
+						this.props.age
+					),
+					_react2.default.createElement(
+						"p",
+						null,
+						"User Object => Name: ",
+						this.props.user.name
+					),
+					_react2.default.createElement(
+						"div",
+						null,
+						_react2.default.createElement(
+							"h4",
+							null,
+							"Hobbies"
+						),
+						_react2.default.createElement(
+							"ul",
+							null,
+							this.props.user.hobbies.map(function (hobby, i) {
+								return _react2.default.createElement(
+									"li",
+									{ key: i },
+									hobby
+								);
+							})
+						)
+					),
+					_react2.default.createElement("hr", null),
+					this.props.children
+				);
+			}
+		}]);
+	
+		return Home;
+	}(_react2.default.Component);
+	
+	//PropTypes enforce that your components get used correctly
+	
+	Home.propTypes = {
+		name: _react2.default.PropTypes.string,
+		age: _react2.default.PropTypes.number,
+		user: _react2.default.PropTypes.object,
+		children: _react2.default.PropTypes.element.isRequired
+	};
 
 /***/ }
 /******/ ]);
