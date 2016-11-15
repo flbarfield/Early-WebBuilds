ce54157529bfc54b6ac2a19cef48f9e7f9edde1a
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-15 08:13:06 -0700

    async actions

--- redux_basics/src/app/actions/userActions.js
+++ redux_basics/src/app/actions/userActions.js
@@ -1,9 +1,24 @@
 // actions are things your going to be dispatching later on
 
 export function setName(name) {
+  ///// How to set a timeout with thunk middleware.
+  // return dispatch => {
+  //   setTimeout(() => {
+  //     dispatch({
+  //       type:"SET_NAME",
+  //       payload: name
+  //     });
+  //   },2000);
+  // }
+
+  ///Timeout with promise package////
   return {
-    type: "SET_NAME",
-    payload: name
+    type:"SET_NAME",
+    payload: new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(name);
+      }, 2000);
+    })
   };
 }
 
