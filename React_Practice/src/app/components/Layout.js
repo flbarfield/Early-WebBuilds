1ce74a93aa91de27fe9ef4d2616c5cc6d22b7e81
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-15 13:31:58 -0700

    finally getting somewhere

---
+++ React_Practice/src/app/components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header"
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    );
+  }
+}
