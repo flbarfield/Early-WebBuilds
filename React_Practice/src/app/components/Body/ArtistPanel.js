2fe840ddb85a04520d39ee366639f3610bb90333
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-16 14:54:51 -0700

    fixed alignments for body

--- React_Practice/src/app/components/Body/ArtistPanel.js
+++ React_Practice/src/app/components/Body/ArtistPanel.js
@@ -5,14 +5,20 @@ import Pan_image from "./ArtistPanel/Pan_image"
 export default class ArtistPanel extends React.Component {
   render() {
     return (
-      <div className="artist-panel col-xs-4">
-        <div className="mu-img-wrap">
-          <Pan_image pan_image={this.props.pan_image}/>
-        </div>
-        <div className="mu-info">
-          <h3 className="artist">Lorem ipsum dolor</h3>
-          <p className="song">Nunc sed erat ac diam</p>
-          <div className="ctrl-btns"></div>
+      <div className="artist-panel col-xs-12 col-md-4">
+        <div className="inner-artist-wrap col-xs-12">
+          <div className="mu-img-wrap col-xs-12">
+            <Pan_image pan_image={this.props.pan_image}/>
+          </div>
+          <div className="mu-info col-xs-12">
+            <h3 className="artist">Lorem ipsum dolor</h3>
+            <p className="song">Nunc sed erat ac diam</p>
+            <div className="ctrl-btn">
+              <i className="fa fa-play col-xs-4">Play</i>
+              <span className="runtime col-xs-4">05:05</span>
+              <i className="fa fa-download col-xs-4">Download</i>
+            </div>
+          </div>
         </div>
       </div>
     );
