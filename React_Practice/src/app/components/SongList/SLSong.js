113e2559d5931c4815bbeb9a01ce29a4c511b688
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-19 09:39:11 -0700

    getting ready to add some states

--- React_Practice/src/app/components/SongList/SLSong.js
+++ React_Practice/src/app/components/SongList/SLSong.js
@@ -6,12 +6,12 @@ export default class SLSong extends React.Component {
 
     return(
       <div className="song col-xs-12">
-        <div className="so-img-wrap col-xs-3">
+        <div className="so-img-wrap col-xs-12 col-md-3">
           {this.props.sl_song}
         </div>
-        <div className="so-playbar col-xs-8">
+        <div className="so-playbar col-md-8">
           {wave_img}
-          <div className="song-bot col-xs-12">
+          <div className="song-bot col-md-12">
             <i className="fa fa-play"></i><span>5:53</span>
             <i className="fa fa-headphones"></i><span>958,985</span>
             <i className="fa fa-heart"></i><span>1,345</span>
