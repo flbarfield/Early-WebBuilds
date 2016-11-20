d4b0eb28bec565f1551f535cc88ce42370c72c7b
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-20 07:59:22 -0700

    got rid of the terrible placeholder for song archive, added songs, fixed header

--- React_Practice/src/app/components/Layout.js
+++ React_Practice/src/app/components/Layout.js
@@ -6,21 +6,6 @@ import {Router, Route, browserHistory, IndexRoute} from "react-router";
 import SongList from "./SongList";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Welcome to Musicitude.",
-  };
-}
-
-  changeTitle(title) {
-    this.setState({title});
-  }
-//// code previously in return ////
-// <div>
-//   <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-//   <Body/>
-// </div>
   render() {
 
     return (
--- React_Practice/src/app/components/SongList.js
+++ React_Practice/src/app/components/SongList.js
@@ -10,12 +10,19 @@ export default class SongList extends React.Component {
       <img className="so_img" src="./app/images/masked_animals.jpg"/>
     ]
 
+    const so_audio_list = [
+      <source src="./app/audio/Nymphaeum-part1-2.mp3"></source>,
+      <source src="./app/audio/Extremes.mp3"></source>,
+      <source src="./app/audio/Stargazer.mp3"></source>,
+      <source src="./app/audio/theCalling.mp3"></source>
+    ]
+
     return(
       <div id='song_list'>
-        <SLSong sl_song={so_img_list[0]}/>
-        <SLSong sl_song={so_img_list[1]}/>
-        <SLSong sl_song={so_img_list[2]}/>
-        <SLSong sl_song={so_img_list[3]}/>
+        <SLSong sl_img={so_img_list[0]} sl_song={so_audio_list[0]}/>
+        <SLSong sl_img={so_img_list[1]} sl_song={so_audio_list[1]}/>
+        <SLSong sl_img={so_img_list[2]} sl_song={so_audio_list[2]}/>
+        <SLSong sl_img={so_img_list[3]} sl_song={so_audio_list[3]}/>
       </div>
     );
   }
