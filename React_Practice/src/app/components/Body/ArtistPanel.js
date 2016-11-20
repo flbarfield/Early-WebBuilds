8903e7ba24701df38875028d3aa76410de8157fc
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-19 18:58:39 -0700

    ROUTERS ARE SO COOL buuuut

--- React_Practice/src/app/components/Body/ArtistPanel.js
+++ React_Practice/src/app/components/Body/ArtistPanel.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Pan_image from "./ArtistPanel/Pan_image"
+import {Link} from "react-router";
 
 export default class ArtistPanel extends React.Component {
   render() {
     return (
       <div className="artist-panel col-xs-12 col-md-4">
         <div className="inner-artist-wrap col-xs-12">
-          <div className="mu-img-wrap col-xs-12">
+          <div className="mu-img-wrap col-xs-12"><Link to={"/songs"}>
             <Pan_image pan_image={this.props.pan_image}/>
-          </div>
+          </Link></div>
           <div className="mu-info col-xs-12">
             <h3 className="artist">Lorem ipsum dolor</h3>
             <p className="title">Nunc sed erat ac diam</p>
