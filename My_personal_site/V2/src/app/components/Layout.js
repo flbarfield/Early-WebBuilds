f443a113e5c1a040d53f97505085cbff1a4f130b
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-23 22:23:12 -0700

    beginning stages of integrating react/redux into my portfolio site

---
+++ My_personal_site/V2/src/app/components/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Router, Route, browserHistory, IndexRoute} from "react-router";
+
+export default class Layout extends React.Component {
+  render() {
+    return(
+      <h1>HI</h1>
+    );
+  }
+}
