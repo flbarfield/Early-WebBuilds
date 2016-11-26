3a225904251aaccd1a4c502475630e9db259161b
Fredrick Barfield <flbarfield@yahoo.com>
2016-11-25 19:55:26 -0700

    getting ready to bind actions

--- My_personal_site/V2/src/app/containers/WebImageProject.js
+++ My_personal_site/V2/src/app/containers/WebImageProject.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
+import {selectProject} from "../actions/ActIndex";
 
 class WebImageProjectList extends Component {
   createWebImageItems(){
     return this.props.projList.map((projList) => {
       return(
-        <div key={projList.key}>
+        <div key={projList.key} onClick={() => this.props.selectProject(projList)}>
           <div className='project-wrapper'>
             <div className={projList.imageWrapStyling}>
               <a href='#' className={projList.id}><img className='col-xs-12 col-sm-12 col-md-12' src={projList.img}/></a>
@@ -32,4 +33,8 @@ function mapStatetoProps(state) {
   };
 }
 
-export default connect(mapStatetoProps)(WebImageProjectList);
+function matchDispatchtoProps(dispatch) {
+  return bindActionCreators({selectProject: selectProject}, dispatch)
+}
+
+export default connect(mapStatetoProps, matchDispatchtoProps)(WebImageProjectList);
