b980ae486e6838a84024e317560531c614ecb83e
Fredrick Barfield <flbarfield@yahoo.com>
2016-12-07 12:34:25 -0700

    mobile main page looks waaay better

---
+++ jsSkillBuilding/src/app/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render () {
+    return (
+      <div className="welFooter">
+        <h1>Lorem ipsum</h1>
+        <p>
+          dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet enim eget justo ornare, non laoreet libero pellentesque. Praesent diam odio, dictum non porta sodales, bibendum et lacus. Curabitur diam lacus, pretium eu ipsum vitae, mollis rhoncus elit. In semper id urna vestibulum blandit. Suspendisse luctus dui nisi, quis sagittis lorem accumsan sit amet. Vivamus sed nibh sodales, rhoncus lectus sit amet, semper odio. Vivamus quis nulla vitae tortor lacinia pharetra. Duis ipsum nisl, lobortis eu vulputate faucibus, blandit id purus. Phasellus sapien nulla, ullamcorper at vehicula quis, malesuada nec quam. In eros nisl, varius sed mollis non, consectetur laoreet eros. Aliquam purus ipsum, posuere non risus vel, tempus auctor tortor. Pellentesque pellentesque eget risus quis euismod.
+        </p>
+      </div>
+    );
+  }
+}
