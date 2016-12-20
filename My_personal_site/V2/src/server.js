9f6bd20743a1150a0d133a4f611bb9efd7b205f3
Fredrick Barfield <flbarfield@yahoo.com>
2016-12-19 22:49:17 -0700

    LOTS of headway on webpack config and fixing link errors. Also learning node.js

---
+++ My_personal_site/V2/src/server.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import {Server} from 'http';
+import Express from 'express';
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import {match, RouterContext} from 'react-router';
+import Layout from './app/containers/Layout';
+
+// initialize the server and configure support for ejs templates
+const app = new Express();
+const server = new Server(app);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// define the folder that will be used for static assets
+app.use(Express.static(path.join(__dirname, 'static')));
+
+//universal routing and rendering
+app.get('*' (req, res) => {
+  match(
+    {Layout, location: req.url},
+    (err, redirectLocation, renderProps) => {
+      //in case of error display the error message
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+      //in case of redirect propagate the redirect to the browser
+      if (redirectLocation) {
+        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      }
+      // generate the react markup for the current route
+      let markup;
+      if (renderProps) {
+        //if the current route matched we have renderProps
+        markup = renderToString(<RouterContext{...renderProps}/>);
+      } else {
+        markup = renderToString('Cannot find page!');
+        res.status(404);
+      }
+      //render the index template with the embedded React markup
+      return res.render('index', {markup})
+    }
+  );
+});
+
+//start the server
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
+server.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info('Server running on http://localhost:${port} [{$env}]');
+});
