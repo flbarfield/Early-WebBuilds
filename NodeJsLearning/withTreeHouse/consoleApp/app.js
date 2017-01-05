dd7b9fce8dc2770984a17be439b5e9f7468960db
Fredrick Barfield <flbarfield@yahoo.com>
2017-01-05 09:07:13 -0700

    took a break during tthe holidays but forgot to commit

---
+++ NodeJsLearning/withTreeHouse/consoleApp/app.js
@@ -0,0 +1,7 @@
+var profile = require('./profile');
+
+var users = ['chalkers', 'fredrickbarfield', 'davemcfarland'];
+
+users.forEach(profile.get);
+
+// other command line app ideas = weather app, stock app
